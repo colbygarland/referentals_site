727488b97301e909dd13222812f4b9e101e5c47a
Colby Garland <colbygarland@gmail.com>
2023-05-02 22:34:19 -0600

    initial somethin

---
+++ types/review.d.ts
@@ -0,0 +1,19 @@
+export interface Review {
+  id: number
+  review: string
+  created_by: string | null
+  created_at: string
+  updated_at: string
+  categories: {
+    name: string
+    rating: number
+  }[]
+  rental: {
+    id: number
+    name: string
+    address: string
+    type: string
+    created_at: string
+    updated_at: string
+  }
+}
