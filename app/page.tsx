3b6ce1447d21408cc9f30b26f65f054ad81c121b
Colby Garland <colbygarland@gmail.com>
2023-05-04 22:31:09 -0600

    home page + reviews error handling basic

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,276 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import { Review } from '@/types/review'
-
-const inter = Inter({ subsets: ['latin'] })
-
-export default async function Home() {
-  const response = await fetch(`${process.env.API_URL}/reviews`)
-  const json = await response.json()
-
+/* eslint-disable react/no-unescaped-entities */
+export default function Reviews() {
   return (
-    <main className="h-screen bg-teal-400">
-      {json.map((j: Review) => {
-        return (
-          <div key={j.id}>
-            <p>{j.review}</p>
-            <p>{j.rental.address}</p>
+    <>
+      <main>
+        <section className="py-16 relative">
+          <img
+            src="hero.webp"
+            alt=""
+            className="absolute inset-0 h-full w-full object-cover z-0"
+          />
+          <div className="relative z-10 py-32 lg:py-64 px-6 lg:px-12">
+            <h1 className="text-white text-5xl lg:text-7xl text-center font-bold">
+              Landlords ask for your references. <br />
+              It's time you leave one for them.
+            </h1>
+            <div className="mt-10 flex gap-6 flex-col md:flex-row md:justify-center">
+              <a
+                href=""
+                className="uppercase text-center inline-block px-5 py-3 bg-secondary rounded-md font-bold"
+              >
+                Write a Review
+              </a>
+              <a
+                href=""
+                className="uppercase text-center inline-block px-5 py-3 bg-primary text-white rounded-md font-bold"
+              >
+                Read Reviews
+              </a>
+            </div>
           </div>
-        )
-      })}
-    </main>
+        </section>
+        <section className="bg-primary text-white px-6 py-12 lg:py-20">
+          <div className="flex flex-col md:flex-row gap-6 md:gap-12 max-w-5xl mx-auto">
+            <div className="text-2xl py-6 border-b-2 md:border-b-0 md:border-r-2 md:pr-12 border-white md:flex-2">
+              <p className="">referental</p>
+              <p>
+                <em>[noun]</em> English
+              </p>
+            </div>
+            <div className="md:flex items-center md:flex-1">
+              <p className="text-xl">
+                A place to find transparent references of your rental, including
+                your landlord and rental experience.
+              </p>
+            </div>
+          </div>
+        </section>
+        <section className="px-6 py-12 lg:py-20 text-primary">
+          <h2 className="text-center text-4xl font-bold mb-20">
+            Why use Referental?
+          </h2>
+          <div className="flex flex-col lg:flex-row gap-12 lg:gap-24">
+            <div className="text-center">
+              <img src="t.png" alt="" className="h-20 w-20 mb-6 mx-auto" />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </div>
+            <div className="text-center">
+              <img src="t.png" alt="" className="h-20 w-20 mb-6 mx-auto" />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </div>
+            <div className="text-center">
+              <img src="t.png" alt="" className="h-20 w-20 mb-6 mx-auto" />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </div>
+          </div>
+          <div className="flex justify-center mt-20">
+            <a
+              href=""
+              className="uppercase text-center inline-block px-5 py-3 bg-primary text-white rounded-md font-bold"
+            >
+              Read Reviews
+            </a>
+          </div>
+        </section>
+        <section className="px-6 py-12 lg:py-20 bg-secondary text-primary">
+          <h2 className="text-center text-4xl font-bold mb-16">
+            Explore review rentals near you
+          </h2>
+          <div className="flex gap-2 justify-center mb-6">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              preserveAspectRatio="xMidYMid meet"
+              version="1.0"
+              viewBox="0.0 0.0 501.1 477.0"
+              className="fill-primary w-10 h-10"
+            >
+              <g id="__id124_s163jqww2b">
+                <path
+                  d="M251.2 1.5L328.4 158 501.1 183.1 376.1 305 405.7 477 251.2 395.8 96.6 477 126.1 305 1.2 183.1 173.9 158z"
+                  className="fill-primary"
+                />
+              </g>
+              <g id="__id125_s163jqww2b">
+                <path
+                  d="M250 0L327.3 156.5 500 181.6 375 303.5 404.5 475.5 250 394.3 95.5 475.5 125 303.5 0 181.6 172.7 156.5z"
+                  className="fill-primary"
+                />
+              </g>
+              <g id="__id126_s163jqww2b">
+                <path
+                  d="M250 0L327.3 156.5 500 181.6 375 303.5 404.5 475.5 250 394.3 95.5 475.5 125 303.5 0 181.6 172.7 156.5z"
+                  className="fill-primary"
+                />
+              </g>
+              <g id="__id127_s163jqww2b">
+                <path
+                  d="M250 0L327.3 156.5 500 181.6 375 303.5 404.5 475.5 250 394.3 95.5 475.5 125 303.5 0 181.6 172.7 156.5z"
+                  className="fill-primary"
+                />
+              </g>
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              preserveAspectRatio="xMidYMid meet"
+              version="1.0"
+              viewBox="0.0 0.0 501.1 477.0"
+              className="fill-primary w-10 h-10"
+            >
+              <g id="__id124_s163jqww2b">
+                <path
+                  d="M251.2 1.5L328.4 158 501.1 183.1 376.1 305 405.7 477 251.2 395.8 96.6 477 126.1 305 1.2 183.1 173.9 158z"
+                  className="fill-primary"
+                />
+              </g>
+              <g id="__id125_s163jqww2b">
+                <path
+                  d="M250 0L327.3 156.5 500 181.6 375 303.5 404.5 475.5 250 394.3 95.5 475.5 125 303.5 0 181.6 172.7 156.5z"
+                  className="fill-primary"
+                />
+              </g>
+              <g id="__id126_s163jqww2b">
+                <path
+                  d="M250 0L327.3 156.5 500 181.6 375 303.5 404.5 475.5 250 394.3 95.5 475.5 125 303.5 0 181.6 172.7 156.5z"
+                  className="fill-primary"
+                />
+              </g>
+              <g id="__id127_s163jqww2b">
+                <path
+                  d="M250 0L327.3 156.5 500 181.6 375 303.5 404.5 475.5 250 394.3 95.5 475.5 125 303.5 0 181.6 172.7 156.5z"
+                  className="fill-primary"
+                />
+              </g>
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              preserveAspectRatio="xMidYMid meet"
+              version="1.0"
+              viewBox="0.0 0.0 501.1 477.0"
+              className="fill-primary w-10 h-10"
+            >
+              <g id="__id124_s163jqww2b">
+                <path
+                  d="M251.2 1.5L328.4 158 501.1 183.1 376.1 305 405.7 477 251.2 395.8 96.6 477 126.1 305 1.2 183.1 173.9 158z"
+                  className="fill-primary"
+                />
+              </g>
+              <g id="__id125_s163jqww2b">
+                <path
+                  d="M250 0L327.3 156.5 500 181.6 375 303.5 404.5 475.5 250 394.3 95.5 475.5 125 303.5 0 181.6 172.7 156.5z"
+                  className="fill-primary"
+                />
+              </g>
+              <g id="__id126_s163jqww2b">
+                <path
+                  d="M250 0L327.3 156.5 500 181.6 375 303.5 404.5 475.5 250 394.3 95.5 475.5 125 303.5 0 181.6 172.7 156.5z"
+                  className="fill-primary"
+                />
+              </g>
+              <g id="__id127_s163jqww2b">
+                <path
+                  d="M250 0L327.3 156.5 500 181.6 375 303.5 404.5 475.5 250 394.3 95.5 475.5 125 303.5 0 181.6 172.7 156.5z"
+                  className="fill-primary"
+                />
+              </g>
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              preserveAspectRatio="xMidYMid meet"
+              version="1.0"
+              viewBox="0.0 0.0 501.1 477.0"
+              className="fill-primary w-10 h-10"
+            >
+              <g id="__id124_s163jqww2b">
+                <path
+                  d="M251.2 1.5L328.4 158 501.1 183.1 376.1 305 405.7 477 251.2 395.8 96.6 477 126.1 305 1.2 183.1 173.9 158z"
+                  className="fill-primary"
+                />
+              </g>
+              <g id="__id125_s163jqww2b">
+                <path
+                  d="M250 0L327.3 156.5 500 181.6 375 303.5 404.5 475.5 250 394.3 95.5 475.5 125 303.5 0 181.6 172.7 156.5z"
+                  className="fill-primary"
+                />
+              </g>
+              <g id="__id126_s163jqww2b">
+                <path
+                  d="M250 0L327.3 156.5 500 181.6 375 303.5 404.5 475.5 250 394.3 95.5 475.5 125 303.5 0 181.6 172.7 156.5z"
+                  className="fill-primary"
+                />
+              </g>
+              <g id="__id127_s163jqww2b">
+                <path
+                  d="M250 0L327.3 156.5 500 181.6 375 303.5 404.5 475.5 250 394.3 95.5 475.5 125 303.5 0 181.6 172.7 156.5z"
+                  className="fill-primary"
+                />
+              </g>
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              preserveAspectRatio="xMidYMid meet"
+              version="1.0"
+              viewBox="0.0 0.0 501.1 477.0"
+              className="fill-white w-10 h-10"
+            >
+              <g id="__id124_s163jqww2b">
+                <path
+                  d="M251.2 1.5L328.4 158 501.1 183.1 376.1 305 405.7 477 251.2 395.8 96.6 477 126.1 305 1.2 183.1 173.9 158z"
+                  className="fill-white"
+                />
+              </g>
+              <g id="__id125_s163jqww2b">
+                <path
+                  d="M250 0L327.3 156.5 500 181.6 375 303.5 404.5 475.5 250 394.3 95.5 475.5 125 303.5 0 181.6 172.7 156.5z"
+                  className="fill-white"
+                />
+              </g>
+              <g id="__id126_s163jqww2b">
+                <path
+                  d="M250 0L327.3 156.5 500 181.6 375 303.5 404.5 475.5 250 394.3 95.5 475.5 125 303.5 0 181.6 172.7 156.5z"
+                  className="fill-white"
+                />
+              </g>
+              <g id="__id127_s163jqww2b">
+                <path
+                  d="M250 0L327.3 156.5 500 181.6 375 303.5 404.5 475.5 250 394.3 95.5 475.5 125 303.5 0 181.6 172.7 156.5z"
+                  className="fill-white"
+                />
+              </g>
+            </svg>
+          </div>
+          <div className="text-center max-w-lg mx-auto ">
+            <p className="mb-4">
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua."
+            </p>
+            <p>
+              <small>
+                Colby Garland <br />
+                Grande Prairie, Alberta, Canada
+              </small>
+            </p>
+          </div>
+          <div className="flex justify-center mt-20">
+            <a
+              href=""
+              className="uppercase text-center inline-block px-5 py-3 bg-primary text-white rounded-md font-bold"
+            >
+              Read Reviews
+            </a>
+          </div>
+        </section>
+      </main>
+      <footer className="bg-primary text-white px-6 py-12 lg:py-20"></footer>
+    </>
   )
 }
---
+++ app/reviews/page.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import { Review } from '@/types/review'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default async function Reviews() {
+  const response = await fetch(`${process.env.API_URL}/reviews`)
+  const json = await response.json()
+
+  return (
+    <main className="h-screen bg-teal-400">
+      {json.map((j: Review) => {
+        return (
+          <div key={j.id}>
+            <p>{j.review}</p>
+            <p>{j.rental.address}</p>
+          </div>
+        )
+      })}
+    </main>
+  )
+}
